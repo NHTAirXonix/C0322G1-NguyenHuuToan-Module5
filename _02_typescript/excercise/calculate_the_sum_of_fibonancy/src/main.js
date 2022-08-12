c3f900e036a856f4aa4c9f94b93f31074b73d1fb
NHTAirXonix <toan02041999@gmail.com>
2022-08-12 15:57:49 +0700

    update

--- _02_typescript/excercise/calculate_the_sum_of_fibonancy/src/main.js
+++ _02_typescript/excercise/calculate_the_sum_of_fibonancy/src/main.js
@@ -1,13 +1,11 @@
 function sumCalculat(amount) {
     var sum = fibonancy(amount, 0, 1, 0, 0);
-    return sum;
 }
 function fibonancy(amount, number1, number2, count, sum) {
     if (count == amount) {
         console.log("-------");
         console.log("Amount is: " + amount);
         console.log("Sum is: " + sum);
-        return sum;
     }
     else {
         var total = sum;
@@ -21,4 +19,4 @@ function fibonancy(amount, number1, number2, count, sum) {
     }
 }
 var amount = 10;
-var result = sumCalculat(amount);
+sumCalculat(amount);
